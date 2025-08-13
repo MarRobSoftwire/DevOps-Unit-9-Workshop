d9b6bec4ff076cea5ce8e2d72aa2d75287c543e8
Mark Roberts <Mark.Roberts@softwire.com>
2025-08-13 11:22:04 +0100

    add tfstate blob

--- exercise/providers.tf
+++ exercise/providers.tf
@@ -9,6 +9,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "1-61e0696e-playground-sandbox"
+    storage_account_name = "13082025terraformstate"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
